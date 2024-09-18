c1c139c6b6313f30d64872550fb2c2ff6e2bb5c7
mldxo <miloszmaculewicz@gmail.com>
2024-09-18 11:54:52 +0200

    Remove index- js and css files exception in generation

--- vite.config.js
+++ vite.config.js
@@ -10,22 +10,7 @@ export default defineConfig({
             return "vendor";
           }
         },
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash].[ext]",
       },
-      plugins: [
-        {
-          name: 'exclude-index-files',
-          generateBundle(options, bundle) {
-            for (const fileName of Object.keys(bundle)) {
-              if (fileName.startsWith('assets/index-') && (fileName.endsWith('.js') || fileName.endsWith('.css'))) {
-                delete bundle[fileName];
-              }
-            }
-          }
-        }
-      ]
     },
   },
 });
